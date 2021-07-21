26d048ec5f50ce7df4fd0ba2cb289afbd6a86c1e
omjpro <omjcrow@naver.com>
2021-07-21 17:27:45 +0900

    korea IT

---
+++ src/ch01/Hellojava.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class Hellojava {
+
+	// 메인함수, 코드의 시작점 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, java");
+		System.out.println("안녕하세요,");
+		System.out.println("Sysout 찍고 컨트롤 + 스페이스 바 를 눌러봅시다.");
+	} //end of main
+
+} // end of class 
---
+++ src/ch01/Variable.java
@@ -0,0 +1,40 @@
+package ch01;
+
+public class Variable {
+
+	// starting point of codes
+	public static void main(String[] args) {
+		
+		int age; // 변수를 선언
+		int count; // 변수를 선언 
+		age = 10; // 초기화라고 불림(값을 넣는 행위
+		count = 100;
+		int name = 100; //변수의 선언과 초기화)를 동시에 
+		
+		System.out.println(age);
+		System.out.println(count);
+
+		// 변수의 규칙
+		
+		// 1. 대소문자 구분, 길이 제한 없음 
+//		int test = 10;
+//		int Test = 20;  
+//		test != Test
+
+		// 2. 자바에서 사용하는 예약어는 사용할 수 없다.
+//		int if;  (X)
+//		int for;  (X)
+//		int whille; (X)
+		
+		// 3. 숫자로 시작해서는 안된다.
+//		int 8name;  (X)
+//		int 100name;  (X)
+		
+		// 4. 특수문자는 _ , $ 만 허용한다.
+//		int _name; (O)
+//		int $name; (O)
+//		int na_me$; (O)
+		
+	} // end of main
+
+} // end of class
